4a902a7129f32efa6578e04ca5ae0b1f548f2334
Repix1337 <you@example.com>
2025-02-08 16:58:14 +0100

    working on attack animation

--- app/Spawn.tsx
+++ app/Spawn.tsx
@@ -20,6 +20,7 @@ interface Tower {
 
 const Spawn: React.FC<SpawnProps> = ({ round,setHealthPoints }) => {
   const [enemies, setEnemies] = useState<Enemy[]>([]);
+  const [attackEffects, setAttackEffects] = useState<{ towerPosition: number, enemyPosition: number }[]>([]);
   const [tower, setTower] = useState<Tower[]>([]);
     useEffect(() => {
         if (round === 1) {
@@ -29,7 +30,7 @@ const Spawn: React.FC<SpawnProps> = ({ round,setHealthPoints }) => {
               { id: uuidv4(), position: -7, src: 'enemy1.png', hp: 100 },
             ])
             
-        }, 500)
+        }, 1000)
         return () => clearInterval(interval)
         }
     }, [round])
@@ -84,7 +85,7 @@ const Spawn: React.FC<SpawnProps> = ({ round,setHealthPoints }) => {
     }, [enemies]);
 
     const buyTowers = (event: React.MouseEvent<HTMLImageElement>, siteNumber:number) => {
-      if (round === 1) {
+      if (round === 1 && (event.target as HTMLImageElement).src.includes('buildingSite')) {
         (event.target as HTMLImageElement).src = '/tower1.png';
         setTower((prevTower) => {
           const newTower = { id: uuidv4(), position: 15 * siteNumber, attack: 50, furthestEnemyInRange: null };
@@ -93,16 +94,28 @@ const Spawn: React.FC<SpawnProps> = ({ round,setHealthPoints }) => {
         });
       }
     }
-    const towerAttack = (towerAttack: number, furthestEnemy: Enemy) => {
+    const towerAttack = (tower: Tower, furthestEnemy: Enemy) => {
       setEnemies((prevEnemies) =>
         prevEnemies.map((enemy) => {
           if (furthestEnemy && enemy.id === furthestEnemy.id) {
-            return { ...enemy, hp: enemy.hp - towerAttack };
+            setAttackEffects((prevAttackEffects) => [...prevAttackEffects, { towerPosition: tower.position, enemyPosition: furthestEnemy.position }]);
+            return { ...enemy, hp: enemy.hp - tower.attack };
           }
           return enemy;
         })
       );
     };
+    const AttackAnimation: React.FC<{ towerPosition: number, enemyPosition: number, onAnimationEnd: () => void }> = ({ towerPosition, enemyPosition, onAnimationEnd }) => {
+      return (
+        <div
+          className='z-20 animate-slide absolute text-red-500'
+          style={{ '--tower-position': `${towerPosition}%`, '--enemy-position': `${enemyPosition}%`, top: '20%' } as React.CSSProperties}
+          onAnimationEnd={onAnimationEnd}
+        >
+          hit
+        </div>
+      )
+    }
 
     useEffect(() => {
       setTower((prevTowers) =>
@@ -121,7 +134,7 @@ const Spawn: React.FC<SpawnProps> = ({ round,setHealthPoints }) => {
         setTower((prevTowers) =>
           prevTowers.map((tower) => {
             if (tower.furthestEnemyInRange) {
-              towerAttack(tower.attack, tower.furthestEnemyInRange);
+              towerAttack(tower, tower.furthestEnemyInRange);
             }
             const furthestEnemy = getFurthestEnemyInRadius(tower.position, 10);
             return furthestEnemy?.id !== tower.furthestEnemyInRange?.id
@@ -134,6 +147,13 @@ const Spawn: React.FC<SpawnProps> = ({ round,setHealthPoints }) => {
       return () => clearInterval(interval);
     }, []); 
     
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setAttackEffects((prevAttackEffects) => prevAttackEffects.filter((_, i) => i !== 0));
+      }, 1000);
+    
+      return () => clearInterval(interval);
+    }, [attackEffects]);
 
     const getFurthestEnemyInRadius = (towerPosition: number, radius: number) => {
       const enemiesInRadius = enemies.filter(
@@ -156,9 +176,17 @@ const Spawn: React.FC<SpawnProps> = ({ round,setHealthPoints }) => {
     <img src='/buildingSite.png' className='absolute top-[20%] left-[45%] w-20 h-20 z-10' onClick={(event) => buyTowers(event, 3)}/>
     <img src='/buildingSite.png' className='absolute top-[20%] left-[60%] w-20 h-20 z-10' onClick={(event) => buyTowers(event, 4)}/>
     {createEnemy()}
+    {attackEffects.map((effect, index) => (
+      <AttackAnimation
+        key={index}
+        towerPosition={effect.towerPosition}
+        enemyPosition={effect.enemyPosition}
+        onAnimationEnd={() => {
+          setAttackEffects((prevAttackEffects) => prevAttackEffects.filter((_, i) => i !== index));
+        }}
+      />
+    ))}
   </div>
-      
-      
   )
 }
 
