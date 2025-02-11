72acb789f7ff144b626960b07368869df0fe3f89
Repix1337 <r3pig959@gmail.com>
2025-02-11 10:15:26 +0100

    attack types new enemy and rapid shooter

--- app/Spawn.tsx
+++ app/Spawn.tsx
@@ -29,13 +29,14 @@ interface Tower {
   position: number;
   attack: number;
   attackSpeed: number;
-  furthestEnemyInRange: Enemy | null;
+  furthestEnemyInRange: Enemy[] | null;
   isAttacking: boolean;
   price: number;
   type: string;
   maxDamage: number;
   maxAttackSpeed: number;
   radius: number;
+  attackType: string;
 }
 
 const Spawn: React.FC<SpawnProps> = ({ round, setHealthPoints, money, setMoney, setRound, hp }) => {
@@ -77,7 +78,7 @@ const Spawn: React.FC<SpawnProps> = ({ round, setHealthPoints, money, setMoney, 
     if (!isPageVisible) return; // Stop if page is not visible
 
     const interval = setInterval(() => {
-    if (round > 0 && enemyCount < 20 * round) {
+    if (round > 0 && round <= 9 && enemyCount < 15 * round) {
        if (enemyCount % 5 !== 0) {
         setEnemies((prevEnemy) => [
           ...prevEnemy,
@@ -94,6 +95,23 @@ const Spawn: React.FC<SpawnProps> = ({ round, setHealthPoints, money, setMoney, 
         setEnemyCount((prevCount) => prevCount + 1);
       }
     }
+    else if (round >= 10 && enemyCount < 15 * round) {
+      if (enemyCount % 5 !== 0) {
+       setEnemies((prevEnemy) => [
+         ...prevEnemy,
+         { id: uuidv4(), position: -7, src: 'enemy3.png', hp: 200, damage: 5, type: 'basic' },
+       ]);
+       setEnemyCount((prevCount) => prevCount + 1);
+     }
+     else if (enemyCount % 5 === 0)
+     {
+       setEnemies((prevEnemy) => [
+         ...prevEnemy,
+         { id: uuidv4(), position: -7, src: 'enemy2.png', hp: 50, damage: 10, type: 'stealth' },
+       ]);
+       setEnemyCount((prevCount) => prevCount + 1);
+     }
+   }
     else if (round === 0) {
       setEnemies([]);
     }
@@ -111,8 +129,8 @@ const Spawn: React.FC<SpawnProps> = ({ round, setHealthPoints, money, setMoney, 
       setShowUpgradeMenu(false);
       const tower1Elements = document.querySelectorAll('img[src="/tower1.png"]') as NodeListOf<HTMLImageElement>;
       const tower2Elements = document.querySelectorAll('img[src="/tower2.png"]') as NodeListOf<HTMLImageElement>;
-
-      [...tower1Elements, ...tower2Elements].forEach((element) => {
+      const tower3Elements = document.querySelectorAll('img[src="/rapidShooter.png"]') as NodeListOf<HTMLImageElement>;
+      [...tower1Elements, ...tower2Elements, ...tower3Elements].forEach((element) => {
         element.src = '/buildingSite.png';
       });
     }
@@ -129,7 +147,7 @@ const Spawn: React.FC<SpawnProps> = ({ round, setHealthPoints, money, setMoney, 
   }, [round, isPageVisible]); // Add isPageVisible to dependencies
 
   // Main tower attack logic
-  const towerAttack = useCallback((tower: Tower, furthestEnemy: Enemy) => {
+  const towerAttack = useCallback((tower: Tower, targets: Enemy[]) => {
     if (tower.isAttacking) return;
    
     setTower((prevTowers) =>
@@ -138,18 +156,18 @@ const Spawn: React.FC<SpawnProps> = ({ round, setHealthPoints, money, setMoney, 
       )
     );
   
-    const newEffect = { 
-      id: uuidv4(), 
-      towerPosition: tower.position, 
-      enemyPosition: furthestEnemy.position, 
-      timestamp: Date.now() 
-    };
+    const newEffects = targets.map(target => ({
+      id: uuidv4(),
+      towerPosition: tower.position,
+      enemyPosition: target.position,
+      timestamp: Date.now()
+    }));
     
-    setAttackEffects((prevEffects) => [...prevEffects, newEffect]);
-    setMoney((prevMoney) => prevMoney + tower.attack / 5);
+    setAttackEffects((prevEffects) => [...prevEffects, ...newEffects]);
+    setMoney((prevMoney) => prevMoney + (tower.attack * targets.length) / 5);
     setTimeout(() => {
       setAttackEffects((prevEffects) => 
-        prevEffects.filter((effect) => effect.id !== newEffect.id)
+        prevEffects.filter((effect) => !newEffects.find(e => e.id === effect.id))
       );
       setTower((prevTowers) =>
         prevTowers.map((t) =>
@@ -160,7 +178,8 @@ const Spawn: React.FC<SpawnProps> = ({ round, setHealthPoints, money, setMoney, 
   
     setEnemies((prevEnemies) =>
       prevEnemies.map((enemy) => {
-        if (furthestEnemy && enemy.id === furthestEnemy.id) {
+        const isTargeted = targets.some(target => target.id === enemy.id);
+        if (isTargeted) {
           return { ...enemy, hp: enemy.hp - tower.attack };
         }
         return enemy;
@@ -176,7 +195,7 @@ const Spawn: React.FC<SpawnProps> = ({ round, setHealthPoints, money, setMoney, 
     setTower((prevTowers) =>
       prevTowers.map((tower) => ({
         ...tower,
-        furthestEnemyInRange: getFurthestEnemyInRadius(tower.position, tower.radius, tower.type) ?? null
+        furthestEnemyInRange: getFurthestEnemyInRadius(tower.position, tower.radius, tower.type, tower.attackType) ?? null
       })
       )
     );
@@ -271,7 +290,8 @@ const Spawn: React.FC<SpawnProps> = ({ round, setHealthPoints, money, setMoney, 
           type: type,
           maxDamage: 100,
           maxAttackSpeed: 300,
-          radius: 10
+          radius: 10,
+          attackType: 'single'
         };
         return [...prevTower, newTower];
       });
@@ -291,8 +311,31 @@ const Spawn: React.FC<SpawnProps> = ({ round, setHealthPoints, money, setMoney, 
           price: 200, 
           type: type,
           maxDamage: 100,
-          maxAttackSpeed: 1000,
-          radius: 80
+          maxAttackSpeed: 800,
+          radius: 80,
+          attackType: 'single'
+        };
+        return [...prevTower, newTower];
+      });
+    }
+    else if (type === 'rapidShooter' && money >= 500) {
+      selectedTower[0].element.src = "/rapidshooter.png";
+      setShowTowerSelectMenu(false);
+      setMoney((prevMoney) => prevMoney - 200);
+      setTower((prevTower) => {
+        const newTower = { 
+          id: newTowerId,  // Use the same ID
+          position: selectedTower[0].towerPosition, 
+          attack: 25, 
+          attackSpeed: 400,
+          furthestEnemyInRange: null, 
+          isAttacking: false, 
+          price: 200, 
+          type: type,
+          maxDamage: 50,
+          maxAttackSpeed: 200,
+          radius: 15,
+          attackType: 'double'
         };
         return [...prevTower, newTower];
       });
@@ -336,6 +379,32 @@ const closeTowerSelectMenu = () => {
                   Max Attack Speed Reached
                 </div>
               )}
+              {selectedTower.type === "basic" && selectedTower.attackType === 'single' ? (
+                <button 
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+                  onClick={upgradeDoubleAttack}
+                  disabled={money < 1000}
+                >
+                  Upgrade Double Attack (1000$)
+                </button>
+              ) : selectedTower.type === "basic" ?(
+                <div className="bg-gray-500 text-white font-bold py-2 px-4 rounded w-full text-center">
+                  Already have double attack
+                </div>
+              ) : null}
+              {selectedTower.type === "rapidShooter" && selectedTower.attackType === 'double' ? (
+                <button 
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+                  onClick={upgradeToTrippleAttack}
+                  disabled={money < 2000}
+                >
+                  Upgrade Tripple Attack (2000$)
+                </button>
+              ) : selectedTower.type === "rapidShooter" ?(
+                <div className="bg-gray-500 text-white font-bold py-2 px-4 rounded w-full text-center">
+                  Already have Tripple Attack
+                </div>
+              ) : null}
               <button 
                 className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-full mt-2"
                 onClick={closeUpgradeMenu}
@@ -375,6 +444,26 @@ const closeTowerSelectMenu = () => {
       );
     }
   }
+  const upgradeDoubleAttack = () => {
+    if (money >= 1000){
+      setMoney((prevMoney) => prevMoney - 1000);
+      setTower((prevTower) => 
+        prevTower.map((t) =>
+          t.id === selectedTowerID ? { ...t,  attackType: 'double' } : t
+        )
+      );
+    }
+  }
+  const upgradeToTrippleAttack = () => {
+    if (money >= 2000){
+      setMoney((prevMoney) => prevMoney - 2000);
+      setTower((prevTower) => 
+        prevTower.map((t) =>
+          t.id === selectedTowerID ? { ...t, attackType: 'triple'} : t
+        )
+      );
+    }
+  }
   // Component for attack animation
   const attackAnimation = () => {
     return attackEffects.map((effect) => (
@@ -396,20 +485,35 @@ const closeTowerSelectMenu = () => {
   
 
   // Get the furthest enemy within a certain radius from the tower
-  const getFurthestEnemyInRadius = (towerPosition: number, radius: number, type: string) => {
-    
-    const enemiesInRadius = enemies.filter(
-      (enemy) => enemy.position <= towerPosition + radius && enemy.position >= towerPosition - radius 
-      && type === "sniper" ? enemy.type === "stealth" || enemy.type != "stealth"  : enemy.type != "stealth" 
-    );
-
+  const getFurthestEnemyInRadius = (towerPosition: number, radius: number, type: string, attackType: string) => {
+    const enemiesInRadius = enemies.filter((enemy) => {
+      const isInRange = enemy.position <= towerPosition + radius && 
+                       enemy.position >= towerPosition - radius;
+      
+      if (type === "sniper") {
+          return isInRange;
+      } else {
+          return isInRange && enemy.type !== "stealth";
+      }
+    });
+  
     if (enemiesInRadius.length === 0) {
       return null;
     }
-
-    return enemiesInRadius.reduce((maxEnemy, currentEnemy) => {
-      return currentEnemy.position > maxEnemy.position ? currentEnemy : maxEnemy;
-    }, enemiesInRadius[0]);
+  
+    // Sort all enemies by position in descending order
+    const sortedEnemies = enemiesInRadius.sort((a, b) => b.position - a.position);
+  
+    // Return array of enemies based on attackType
+    if (attackType === 'double' && sortedEnemies.length >= 2) {
+      return sortedEnemies.slice(0, 2);
+    } 
+    else if (attackType === 'double' && sortedEnemies.length >= 3)
+    {
+      return sortedEnemies.slice(0, 3);
+    } else {
+      return [sortedEnemies[0]];
+    }
   }
   return (
     <div className='relative h-4/5 border border-white overflow-hidden'>
@@ -426,13 +530,25 @@ const closeTowerSelectMenu = () => {
       {showTowerSelectMenu && (
       <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-40 bg-slate-400 flex flex-col items-center justify-center p-4 rounded-lg gap-2'>
         <h1 className="text-xl font-bold mb-2">Tower Select Menu</h1>
-        <div>
-          <button onClick={() => selectTowerType("basic",selectedTowerID)}>
-            <img src='/tower1.png' className='w-16 h-16'/>
-          </button>
-          <button onClick={() => selectTowerType("sniper",selectedTowerID)}>
-            <img src='/tower2.png' className='w-16 h-16'/>
-          </button>
+        <div className='flex gap-4'>
+          <div>
+            <button onClick={() => selectTowerType("basic",selectedTowerID)}>
+              <img src='/tower1.png' className='w-16 h-16'/>
+            </button>
+            <h1>100$</h1>
+          </div>
+          <div>
+            <button onClick={() => selectTowerType("sniper",selectedTowerID)}>
+              <img src='/tower2.png' className='w-16 h-16'/>
+            </button>
+            <h1>200$</h1>
+          </div>
+          <div>
+            <button onClick={() => selectTowerType("rapidShooter",selectedTowerID)}>
+              <img src='/rapidShooter.png' className='w-16 h-16'/>
+            </button>
+            <h1>500$</h1>
+          </div>
         </div>
         <button 
               className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-full mt-2"
