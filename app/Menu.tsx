aa0ee6abb46f43d65e8634d0aaa31ecb5147520f
Repix1337 <r3pig959@gmail.com>
2025-02-13 14:17:58 +0100

    New UI

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -15,13 +15,14 @@ const Menu = () => {
     
   return (
     <div>
-    <Spawn round={round} setHealthPoints={setHealthPoints} money={money} setMoney={setMoney} setRound={setRound} hp={HealthPoints}/>
-    <div className='flex flex-col justify-center items-center'>
-    <div className=' text-red hover:cursor-pointer' onClick={onClick}>Start</div>
+      <div className='flex justify-center items-center gap-3 text-xl bg-slate-700 h-12 '>
+    <div className=' bg-slate-800 p-1 text-xl rounded-lg shadow-sm hover:cursor-pointer' onClick={onClick}>Start</div>
     <div>Round: {round}/30</div>
-    <div>Health Points: {HealthPoints}</div>
-    <div>Money: {money}</div>
+    <div className='text-red-500 text-xl'> &hearts; {HealthPoints}</div>
+    <div className=' text-green-500'>{money}$</div>
     </div>
+    <Spawn round={round} setHealthPoints={setHealthPoints} money={money} setMoney={setMoney} setRound={setRound} hp={HealthPoints}/>
+    
     </div>
   )
 }
