43280513003d65f3ac98083bdb81328a58a5a45f
Repix1337 <r3pig959@gmail.com>
2025-03-05 15:45:43 +0100

    .

--- app/saveGameResult.tsx
+++ app/saveGameResult.tsx
@@ -1,16 +1,16 @@
 import { db } from '@/firebase/config';
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { collection, addDoc, serverTimestamp, Timestamp } from 'firebase/firestore';
 
 interface GameResult {
   roundRecord: number;
-  timestamp: any;
+  timestamp: Timestamp;
 }
 
 export const saveGameResult = async (round: number): Promise<void> => {
   try {
     const gameResult: GameResult = {
       roundRecord: round,
-      timestamp: serverTimestamp(),
+      timestamp: serverTimestamp() as Timestamp,
     };
     
     await addDoc(collection(db, 'leaderboard'), gameResult);
